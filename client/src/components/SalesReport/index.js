a39c402aa5af7c693f66119efbe59cfa8e8143b5
John Herman <hermanjohn2@gmail.com>
2021-07-07 17:49:12 -0400

    Initialized sales filter

--- client/src/components/SalesReport/index.js
+++ client/src/components/SalesReport/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form, Select, Radio, Icon } from 'semantic-ui-react';
 
 const SalesReport = props => {
 	const paidJobs = props.data.jobs.filter(job => job.datePaid);
@@ -13,9 +14,60 @@ const SalesReport = props => {
 	const [totalCustomers, setTotalCustomers] = useState(
 		props.data.customers.length
 	);
+	const [showFilter, setShowFilter] = useState(false);
+	const [showAdvancedFilter, setShowAdvancedFilter] = useState(false);
+
+	const filterOptions = [
+		{ key: 'filter-option-all', text: 'All Dates', value: 'all-dates' },
+		{ key: 'filter-past-7', text: 'Past 7 Days', value: 'past-seven' },
+		{ key: 'filter-last-month', text: 'Last Month', value: 'last-month' },
+		{ key: 'filter-ytd', text: 'Year To Date', value: 'ytd' },
+		{ key: 'filter-last-year', text: 'Last Year', value: 'last-year' },
+		{ key: 'filter-advanced', text: 'Advanced', value: 'advanced' }
+	];
+
+	const handleFirstFilter = selection => {
+		switch (selection) {
+			case 'advanced':
+				setShowFilter(false);
+				setShowAdvancedFilter(true);
+				break;
+
+			default:
+				console.log(selection);
+				break;
+		}
+	};
 
 	return (
 		<div>
+			<Form>
+				<Icon
+					name="filter"
+					size="large"
+					onClick={() =>
+						!showFilter ? setShowFilter(true) : setShowFilter(false)
+					}
+					checked={showFilter}
+				/>
+
+				<Form.Group widths="equal">
+					{showFilter ? (
+						<Form.Field
+							control={Select}
+							options={filterOptions}
+							label="Filter Options"
+							onChange={(e, data) => handleFirstFilter(data.value)}
+						/>
+					) : null}
+					{showAdvancedFilter ? (
+						<>
+							<Form.Field control={Select} label="By Month" />
+							<Form.Field control={Select} label="By Year" />
+						</>
+					) : null}
+				</Form.Group>
+			</Form>
 			<h1>Gross Sales - ${grossSales}</h1>
 			<h1>Paid Jobs - {totalJobs}</h1>
 			<h1>Total Customers - {totalCustomers}</h1>
